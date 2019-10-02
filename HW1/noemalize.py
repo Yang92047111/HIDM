301b928d115d609e5ac82ad299d3f80426a762d7
Yang <okm92047111@gmail.com>
2019-10-02 15:24:11 +0800

    complex exercises's answer

--- HW1/noemalize.py
+++ HW1/noemalize.py
@@ -1,4 +1,5 @@
 from numpy import std, cov, mean
+from scipy import stats
 
 age_dataset = [23, 23, 27, 27, 39, 41, 47, 49, 50, 52, 54, 54, 56, 57, 58, 58, 60, 61]
 fat_dataset = [9.5, 26.5, 7.8, 17.8, 31.4, 25.9, 27.4, 27.2, 31.2, 34.6, 42.5, 28.8, 33.4, 30.2, 34.1, 32.9, 41.2, 35.7]
@@ -21,3 +22,4 @@ if __name__ == "__main__":
     print(z_score(age_dataset))
     print(covariance(age_dataset, fat_dataset))
     print(pearson(age_dataset, fat_dataset))
+    print(stats.pearsonr(age_dataset, fat_dataset))
